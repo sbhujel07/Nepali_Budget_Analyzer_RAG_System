88fdcbb5e5dbd36911ff17913ea659d17ae49ee9
Sandip Bhujel <bhujelsandip07@gmail.com>
2026-08-13 15:36:51 +0545

    update setting

--- backend/app/setting.py
+++ backend/app/setting.py
@@ -16,3 +16,4 @@ R2_ACCESS_KEY_ID = os.getenv("R2_ACCESS_KEY_ID")
 R2_SECRET_ACCESS_KEY = os.getenv("R2_SECRET_ACCESS_KEY")
 R2_BUCKET_NAME = os.getenv("R2_BUCKET_NAME")
 R2_ENDPOINT = f"https://{R2_ACCOUNT_ID}.r2.cloudflarestorage.com"
+HF_TOKEN = os.getenv("HF_TOKEN")
