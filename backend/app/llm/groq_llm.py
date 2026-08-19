617f6c0a52042bfba94e284896ce680aca30dd0c
Sandip Bhujel <bhujelsandip07@gmail.com>
2026-08-19 15:35:45 +0545

    Added the OpenAi model

--- backend/app/llm/groq_llm.py
+++ backend/app/llm/groq_llm.py
@@ -1,15 +1,39 @@
-#set the environment variable for Groq API key
+# #set the environment variable for Groq API key
+# import os
+# from dotenv import load_dotenv
+# from langchain_openai import ChatOpenAI
+
+# load_dotenv()
+
+# groq_api_key = os.getenv("GROQ_API_KEY")
+
+# if not groq_api_key:
+#     raise ValueError("GROQ_API_KEY is missing")
+
+# llm = ChatOpenAI(model = "gpt-5.4-mini",temperature = 0.1)
+# response = llm.invoke("नेपालमा सबैभन्दा अग्लो हिमाल कुन हो??").content
+# print(response)
+
+
 import os
 from dotenv import load_dotenv
-from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
-groq_api_key = os.getenv("GROQ_API_KEY")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+base_url="https://api-direct.apikey.cloud/openai/v1"
 
-if not groq_api_key:
-    raise ValueError("GROQ_API_KEY is missing")
+if not openai_api_key:
+    raise ValueError("OPENAI_API_KEY is missing")
 
-llm = ChatGroq(model = "llama-3.1-8b-instant",temperature = 0.1)
+llm = ChatOpenAI(
+    model="gpt-5.4",
+    temperature=0.1,
+    api_key=openai_api_key,
+    base_url=base_url,
+    max_retries=0,
+)
+
 # response = llm.invoke("नेपालमा सबैभन्दा अग्लो हिमाल कुन हो??").content
 # print(response)
--- backend/app/setting.py
+++ backend/app/setting.py
@@ -17,3 +17,5 @@ R2_SECRET_ACCESS_KEY = os.getenv("R2_SECRET_ACCESS_KEY")
 R2_BUCKET_NAME = os.getenv("R2_BUCKET_NAME")
 R2_ENDPOINT = f"https://{R2_ACCOUNT_ID}.r2.cloudflarestorage.com"
 HF_TOKEN = os.getenv("HF_TOKEN")
+
+
